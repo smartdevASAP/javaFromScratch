f9f838a147c98fba4dd44dd7738fd33c549ab3b8
kelvinTech <kelvinnjuguna357@gmail.com>
2025-11-03 21:50:24 +0300

    io

---
+++ io.java
@@ -0,0 +1,9 @@
+public class io{
+public static  void main(String[] args){
+IO.println("Hello world");
+var name=IO.readln("What is your name: ");
+var city=IO.readln("where do you come from: ");
+IO.print("You are "+name+" from "+city);
+}
+
+}

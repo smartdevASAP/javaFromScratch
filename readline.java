8a2d4767f2ed6a552dda1ef3df27504002b7e6b9
kelvinTech <kelvinnjuguna357@gmail.com>
2025-11-03 21:36:03 +0300

    adding 2 lines

--- readline.java
+++ readline.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 public class readline{
 public static void main(String[] args){
+
 Scanner scanner=new Scanner(System.in);
 System.out.println("Enter a number: ");
-int myNum=scanner.nextInt();
-System.out.println("You entered: "+myNum);
+int num=scanner.nextInt();
+System.out.println("enter a second number: ");
+int num2=scanner.nextInt();
+int result=num+num2;
+System.out.println("Answer is: "+result);
 scanner.close();
 }
 }
